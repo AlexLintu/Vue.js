1685ada971f8e70224505fa5a79493b2ecaee9b2
Web Intern <web.intern@canucks.com>
2019-04-29 13:50:36 -0700

    Add dynamic data

--- 3-blog-app/app.js
+++ 3-blog-app/app.js
@@ -2,12 +2,13 @@ const app = new Vue({
   el: '#vue-app',
   data: {
     name: 'Alex',
-    job: 'Dev'
+    job: 'Dev',
+    website: 'http'
   },
 
   methods: {
-    greet: function () {
-      return 'Good morning!';
+    greet: function (time) {
+      return 'Good ' + time + ' ' + this.name + '!';
     }
   }
 });
