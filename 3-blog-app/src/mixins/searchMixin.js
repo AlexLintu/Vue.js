73ff54f68e5d12df1556f08b0791ccabed774dcd
Web Intern <web.intern@canucks.com>
2019-05-03 09:02:14 -0700

    Move the project to private repo

--- 3-blog-app/src/mixins/searchMixin.js
+++
@@ -1,9 +0,0 @@
-export default {
-  computed: {
-    filteredClients: function () {
-      return this.clients.filter((client) => {
-        return client.name.match(this.search);
-      });
-    }
-  }
-};
